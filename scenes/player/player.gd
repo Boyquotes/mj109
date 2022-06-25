ae98f3140869c088a52a67bc0622ca83766ba46f
Emily <tweetychrip@gmail.com>
2022-06-25 09:09:43 -0700

    Add player char

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,4 +1,12 @@
 extends KinematicBody2D
 
 
-export var speed := 200
+export var base_speed := 200
+export var size := 1.0
+
+
+func _physics_process(_delta: float) -> void:
+	var input := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	move_and_slide(base_speed * size * input)
+	if not input.x == 0:
+		$Sprite.flip_h = input.x < 0
